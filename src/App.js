8572bf9a62cd1f76331367abbfb3741b830cf341
mohammedazhar.1598 <mohammedazhar.1598@gmail.com>
2021-11-26 19:34:34 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './App.css'
+import countries from './data'
 import axios from 'axios'
 
 function App() {
@@ -7,63 +8,6 @@ function App() {
   const [data, setData] = React.useState([])
   const[country,setCountry]=React.useState('nl')
 
-  let countries=[
-    {name:'Argentina',code:'ar'},
-    {name:'Australia',code:'au'},
-    {name:'Austria',code:'at'},
-    {name:'Belgium',code:'be'},
-    {name:'Brazil',code:'br'},
-    {name:'Bulgaria',code:'bg'},
-    {name:'Canada',code:'ca'},
-    {name:'China',code:'cn'},
-    {name:'Colombia',code:'co'},
-    {name:'Cuba',code:'cu'},
-    {name:'Czech Republic',code:'cz'},
-    {name:'Egypt',code:'eg'},
-    {name:'France',code:'fr'},
-    {name:'Germany',code:'de'},
-    {name:'Greece',code:'gr'},
-    {name:'Hong Kong',code:'hk'},
-    {name:'Hungary',code:'hu'},
-    {name:'India',code:'in'},
-    {name:'Indonesia',code:'id'},
-    {name:'Ireland',code:'ie'},
-    {name:'Japan',code:'jp'},
-    {name:'Latvia',code:'lv'},
-    {name:'Lithuania',code:'lt'},
-    {name:'Malaysia',code:'my'},
-    {name:'Mexico',code:'mx'},
-    {name:'Morocco',code:'ma'},
-    {name:'Netherlands',code:'nl'},
-    {name:'New Zealand',code:'nz'},
-    {name:'Nigeria',code:'ng'},
-    {name:'Norway',code:'no'},
-    {name:'Philippines',code:'ph'},
-    {name:'Poland',code:'pl'},
-    {name:'Portugal',code:'pt'},
-    {name:'Romania',code:'ro'},
-    {name:'Russia',code:'ru'},
-    {name:'Saudi Arabia',code:'sa'},
-    {name:'Serbia',code:'rs'},
-    {name:'Singapore',code:'sg'},
-    {name:'Slovakia',code:'sk'},
-    {name:'Slovenia',code:'si'},
-    {name:'South Africa',code:'za'},
-    {name:'South Korea',code:'kr'},
-    {name:'Sweden',code:'se'},
-    {name:'Switzerland',code:'ch'},
-    {name:'Taiwan',code:'tw'},
-    {name:'Thailand',code:'th'},
-    {name:'Turkey',code:'tr'},
-    {name:'UAE',code:'ae'},
-    {name:'Ukraine',code:'ua'},
-    {name:'United Kingdom',code:'gb'},
-    {name:'United States',code:'us'},
-    {name:'Venezuela',code:'ve'},
-  
-
-
-  ]
 
   React.useEffect(() => {
 
@@ -78,8 +22,6 @@ axios.get(`https://newsapi.org/v2/top-headlines?country=${country}&apiKey=972d8c
    setCountry(e.target.value)
    setData([])
   }
-
-  
 
   return (
     <div className="container">
